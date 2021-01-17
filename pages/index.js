9982c9778647f825b84b728826fbe726ca17dc63
yudihermawan <yudihermawan@student.telkomuniversity.ac.id>
2021-01-17 20:28:05 +0700

    Update text

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,20 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
+  var year = new Date().getFullYear() - 2017;
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Yudi Hermawan!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Hi there!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          I am experienced Software Developer trained {year} years of total experience in Web Development using backend stacks: Docker, .NET Core. .NET Framework, Laravel, MySQL, MongoDB, Redis, Object Relational Mapping using Entity Framework Core, RESTful API, OData, unit testing using xUnit, while on frontend stacks using Vue.js, Reach.js, and jQuery. I have also some experiences in SharePoint development. 
         </p>
       </main>
-
-      <Footer />
     </div>
   )
 }
