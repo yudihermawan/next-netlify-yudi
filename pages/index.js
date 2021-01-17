908838ae3e965ed0e47f19b5069f41f802fec9e0
Yudi Hermawan <hello@yudi.me>
2021-01-17 20:44:39 +0700

    made text readable

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,21 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   var year = new Date().getFullYear() - 2017;
   return (
     <div className="container">
       <Head>
-        <title>Yudi Hermawan!</title>
+        <title>Yudi Hermawan</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Hi there!" />
         <p className="description">
-          I am experienced Software Developer trained {year} years of total experience in Web Development using backend stacks: Docker, .NET Core. .NET Framework, Laravel, MySQL, MongoDB, Redis, Object Relational Mapping using Entity Framework Core, RESTful API, OData, unit testing using xUnit, while on frontend stacks using Vue.js, Reach.js, and jQuery. I have also some experiences in SharePoint development. 
+          My name Yudi Hermawan, I am experienced Software Developer trained {year} years of total experience in Web Development using backend stacks: 
+          Docker, .NET Core. .NET Framework, Laravel, MySQL, MongoDB, Redis, Object Relational Mapping using Entity Framework Core, RESTful API, OData, unit testing using xUnit, 
+          while on frontend stacks using Vue.js, Reach.js, and jQuery. I have also some experiences in SharePoint development. 
         </p>
       </main>
     </div>
