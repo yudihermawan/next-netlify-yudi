7b1254678b497f0e798c1b53030e3560a9f60e42
Yudi Hermawan <hello@yudi.me>
2021-03-28 06:22:16 +0700

    fix typo

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,9 @@ export default function Home() {
       <main>
         <Header title="Hi there!" />
         <p className="description">
-          My name Yudi Hermawan, I am experienced Software Developer trained {year} years of total experience in Web Development using backend stacks: 
-          Docker, .NET Core. .NET Framework, Laravel, MySQL, MongoDB, Redis, Object Relational Mapping using Entity Framework Core, RESTful API, OData, unit testing using xUnit, 
-          while on frontend stacks using Vue.js, Reach.js, and jQuery. I have also some experiences in SharePoint development. <br/> <br/> 
+          My name Yudi Hermawan, I am experienced Software Developer trained {year} years of total experience in Web Development using backend stacks:
+          Docker, .NET Core. .NET Framework, Laravel, MySQL, MongoDB, Redis, Object Relational Mapping using Entity Framework Core, RESTful API, OData, unit testing using xUnit,
+          while on frontend stacks using Vue.js, React.js, and jQuery. I have also some experiences in SharePoint development. <br /> <br />
           Find me on <a href="https://github.com/yudihermawan">Github</a>, <a href="https://www.linkedin.com/in/yudi-hermawan/">LinkedIn</a>, <a href="https://medium.com/@yudi.hs">Medium</a>, <a href="https://twitter.com/yudify_">Twitter</a>, <a href="https://facebook.com/hs.yudi">Facebook</a>, or <a href="https://www.instagram.com/yudihermawan.s/">Instagram</a>
         </p>
       </main>
